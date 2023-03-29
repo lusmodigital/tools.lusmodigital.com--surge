70f7158cba1b47c8ddd3d2a09ee4d7fa4ca6ca88
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-03-29 10:36:42 +0700

    add: header, simple image, paragraph, and list editorjs integrations

--- editor.js
+++ editor.js
@@ -1,4 +1,23 @@
 const editor = new EditorJS({
+        onReady: () => {
+            new Undo({ editor });
+            new DragDrop(editor);
+        },
+        tools: {
+            header: Header,
+            image: SimpleImage,
+            paragraph: {
+                class: Paragraph,
+                inlineToolbar: true,
+            },
+            list: {
+                class: List,
+                inlineToolbar: true,
+                config: {
+                  defaultStyle: 'unordered'
+                }
+            },
+        },
         holder: 'editorjs',
     }
 );
