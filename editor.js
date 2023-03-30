f7d4647949de74eb78c2440327106c465a87bdf9
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-03-30 14:35:40 +0700

    add: EditorJS form on fetch article section

--- editor.js
+++ editor.js
@@ -36,10 +36,52 @@ const editor = new EditorJS({
 editor.isReady
   .then(() => {
     document.getElementById('statusText').innerHTML = '<b>Status: </b>Editor.js sudah dapat digunakan!'
-    /** Do anything you need after editor initialization */
   })
   .catch((reason) => {
     document.getElementById('statusText').innerHTML = `<b>Status: </b>Editor.js gagal dijalankan karena ${reason}!`
+});
+
+const editorFetch = new EditorJS({
+        onReady: () => {
+            new Undo({ editor });
+            new DragDrop(editor);
+        },
+        logLevel: 'ERROR',
+        tools: {
+            header: Header,
+            image: SimpleImage,
+            paragraph: {
+                class: Paragraph,
+                inlineToolbar: true,
+            },
+            list: {
+                class: List,
+                inlineToolbar: true,
+                config: {
+                  defaultStyle: 'unordered'
+                }
+            },
+            olist: {
+                class: List,
+                inlineToolbar: true,
+                config: {
+                  defaultStyle: 'ordered'
+                }
+            },
+            markdownParser: MDParser,
+            markdownImporter: MDImporter,
+        },
+        holder: 'editorjs_fetch',
+        minHeight : 0
+    }
+);
+
+editorFetch.isReady
+  .then(() => {
+    document.getElementById('statusTextFetch').innerHTML = '<b>Status: </b>Editor.js sudah dapat digunakan!'
+  })
+  .catch((reason) => {
+    document.getElementById('statusTextFetch').innerHTML = `<b>Status: </b>Editor.js gagal dijalankan karena ${reason}!`
 });
 
 const editorHasil = new EditorJS({
