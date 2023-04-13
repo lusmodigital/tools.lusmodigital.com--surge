4f5d3261dc3cddbd8852a99b1bc6e95e31e0deb2
Raul Ilma Rajasa <raulilmarajasa@gmail.com>
2023-04-13 13:43:13 +0700

    feat: add editor JS to generate final article spin

--- editor.js
+++ editor.js
@@ -83,3 +83,45 @@ editorHasil.isReady
   .catch((reason) => {
     document.getElementById('statusTextHasil').innerHTML = `<b>Status: </b>Editor.js gagal dijalankan karena ${reason}!`
 });
+const editorHasilAkhir = new EditorJS({
+    autofocuse: true,
+    onReady: () => {
+        new Undo({ editor });
+        new DragDrop(editor);
+    },
+    logLevel: 'ERROR',
+    tools: {
+        header: Header,
+        image: SimpleImage,
+        paragraph: {
+            class: Paragraph,
+            inlineToolbar: true,
+        },
+        list: {
+            class: List,
+            inlineToolbar: true,
+            config: {
+              defaultStyle: 'unordered'
+            }
+        },
+        olist: {
+            class: List,
+            inlineToolbar: true,
+            config: {
+              defaultStyle: 'ordered'
+            }
+        },
+        markdownParser: MDParser,
+        markdownImporter: MDImporter,
+    },
+    holder: 'editorjs_hasil_akhir',
+    minHeight : 0
+});
+
+editorHasilAkhir.isReady
+  .then(() => {
+    document.getElementById('statusTextHasilAkhir').innerHTML = '<b>Status: </b>Editor.js sudah dapat digunakan!'
+  })
+  .catch((reason) => {
+    document.getElementById('statusTextHasilAkhir').innerHTML = `<b>Status: </b>Editor.js gagal dijalankan karena ${reason}!`
+});
